0636957cb752f989dd5dc51b0d24bb440df8eecf
François Geerolf <geerolf@yahoo.fr>
2018-03-22 14:27:19 -0700

    Create housing.elasticity.R

---
+++ Saiz2010/housing.elasticity.R
@@ -0,0 +1,16 @@
+pklist <- c("dtplyr", "curl", "foreign")
+source("https://raw.githubusercontent.com/fgeerolf/R/master/load-packages.R")
+
+url.Saiz2010 <- "http://real.wharton.upenn.edu/~gyourko/WRLURI/"
+filename <- "WHARTON LAND REGULATION DATA_1_24_2008"
+filename.html <- gsub(" ", "%20", filename)
+filename.zip <- paste(filename, ".zip", sep = "")
+filename.dta <- paste(filename, ".dta", sep = "")
+
+curl_download(paste(url.Saiz2010, filename.html, ".zip", sep = ""), destfile = filename.zip, quiet = FALSE)
+
+unzip(filename.zip)
+housing.elasticity <- read.dta(filename.dta)
+unlink(filename.zip)
+unlink(filename.dta)
+rm(url.Saiz2010, filename, filename.html, filename.zip, filename.dta)
