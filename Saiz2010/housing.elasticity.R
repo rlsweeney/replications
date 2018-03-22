90518dc07a8c299c7f10cf6445bece02ce571347
François Geerolf <geerolf@yahoo.fr>
2018-03-22 14:34:30 -0700

    Update housing.elasticity.R

--- Saiz2010/housing.elasticity.R
+++ Saiz2010/housing.elasticity.R
@@ -1,16 +1,11 @@
 pklist <- c("dtplyr", "curl", "foreign")
 source("https://raw.githubusercontent.com/fgeerolf/R/master/load-packages.R")
 
-url.Saiz2010 <- "http://real.wharton.upenn.edu/~gyourko/WRLURI/"
-filename <- "WHARTON LAND REGULATION DATA_1_24_2008"
-filename.html <- gsub(" ", "%20", filename)
-filename.zip <- paste(filename, ".zip", sep = "")
-filename.dta <- paste(filename, ".dta", sep = "")
+url.Saiz2010 <- "http://web.archive.org/web/20100619052721/http://real.wharton.upenn.edu/~saiz/"
+filename.zip <- "SUPPLYDATA.zip"
+filename.dta <- "HOUSING_SUPPLY.dta"
 
-curl_download(paste(url.Saiz2010, filename.html, ".zip", sep = ""), destfile = filename.zip, quiet = FALSE)
-
+curl_download(paste(url.Saiz2010, filename.zip, sep = ""), destfile = filename.zip, quiet = FALSE)
 unzip(filename.zip)
-wharton.land.regulation <- read.dta(filename.dta)
-unlink(filename.zip)
-unlink(filename.dta)
-rm(url.Saiz2010, filename, filename.html, filename.zip, filename.dta)
+housing.supply <- read.dta(filename.dta)
+rm(filename.dta, filename.zip, url.Saiz2010)
